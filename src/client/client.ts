77fa5c8b559b1c2756a5db470f460e99c04bd563
John Chavez <jchavez@bandwidth.com>
2020-10-23 16:09:53 -0400

    updates to the game code

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,56 +1,60 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable import/no-extraneous-dependencies */
-import Edc, { TimeoutError, AckedErrorEvent, ClientHandlers } from 'edc-ws'
-import readline from 'readline'
+import Edc from 'edc-ws'
 import prompt from 'prompt-sync'
-import { GuessEvent, RightGuessEvent, WrongGuessEvent, StartGameEvent, UnknownEventErrorEvent } from '../events'
+import { GuessEvent, RightGuessEvent, WrongGuessEvent, StartGameEvent } from '../events'
 
 const port = 8085
 
 const input = prompt()
 
-const clientHandlers: ClientHandlers = {
-    onEvent: async (cause, reply) => {
-        console.log('---------------------------------------')
-        if (cause.type === StartGameEvent.type) {
-            const start = <StartGameEvent>cause
+const client = new Edc.Client(`ws://localhost:${port}`)
 
-            console.log(start.details.title)
-            console.log(start.details.instructions)
-            console.log(`\nHere is your word bank: \n`)
-            console.log(start.shared.notGuessed.join('\n'))
-        } else if (cause.type === RightGuessEvent.type) {
-            const right = <RightGuessEvent>cause
-            const { attempts } = right.shared
-
-            console.log(`Congrats you got it right in ${attempts} attempts!!!`)
-            return
-        } else if (cause.type === WrongGuessEvent.type) {
-            const wrong = <WrongGuessEvent>cause
-
-            console.log(`Sorry Wrong. You have made ${wrong.shared.attempts} attempts.  You have guessed the following`)
-            console.log(wrong.shared.guessed.join('\n'))
-            console.log('\n')
-            console.log('This is what you have left in your wordbank')
-            console.log(wrong.shared.notGuessed.join('\n'))
-        } else {
-            console.log(`Unknown event recieved: ${cause.type}`)
-            reply(new UnknownEventErrorEvent(cause))
-            return
-        }
-
-        console.log('\n')
-        const guessStr = input('What is your is your next guess:  ')
-        console.log('\n')
-
-        const guess = new GuessEvent(guessStr).inherit(cause)
-
-        reply(guess)
-    },
-    onError: async (cause) => {
-        console.log(cause.details.message)
-    },
-    onAck: async () => {}
+client.onError = async (cause) => {
+    console.log(cause.details.message)
 }
 
-const client = new Edc.Client(`ws://localhost:${port}`, clientHandlers)
+client.onEvent(StartGameEvent.type, async (cause, reply) => {
+    console.log('---------------------------------------')
+    const start = <StartGameEvent>cause
+
+    console.log(start.details.title)
+    console.log(start.details.instructions)
+    console.log(`\nHere is your word bank: \n`)
+    console.log(start.shared.notGuessed.join('\n'))
+
+    console.log('\n')
+    const guessStr = input('What is your is your next guess:  ')
+    console.log('\n')
+
+    const guess = new GuessEvent(guessStr).inherit(cause)
+
+    reply(guess)
+})
+
+client.onEvent(WrongGuessEvent.type, async (cause, reply) => {
+    console.log('---------------------------------------')
+    const wrong = <WrongGuessEvent>cause
+
+    console.log(`Sorry Wrong. You have made ${wrong.shared.attempts} attempts.  You have guessed the following`)
+    console.log(wrong.shared.guessed.join('\n'))
+    console.log('\n')
+    console.log('This is what you have left in your wordbank')
+    console.log(wrong.shared.notGuessed.join('\n'))
+
+    console.log('\n')
+    const guessStr = input('What is your is your next guess:  ')
+    console.log('\n')
+
+    const guess = new GuessEvent(guessStr).inherit(cause)
+
+    reply(guess)
+})
+
+client.onEvent(RightGuessEvent.type, async (cause, reply) => {
+    console.log('---------------------------------------')
+    const right = <RightGuessEvent>cause
+    const { attempts } = right.shared
+
+    console.log(`Congrats you got it right in ${attempts} attempts!!!`)
+})
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable import/no-extraneous-dependencies */
-import Edc, { ServerHandlers, BasicAuth, Event } from 'edc-ws'
+import Edc, { BasicAuth, Event } from 'edc-ws'
 import {
     GuessEvent,
     UnknownEventErrorEvent,
@@ -16,40 +16,32 @@ const correct = 'fox'
 
 const wordBank = ['elephant', 'zebra', 'fish', correct, 'dog', 'cat', 'tiger', 'wombat', 'deer']
 
-const serverHandlers: ServerHandlers = {
-    onEvent: async (cause, ws, reply) => {
-        if (cause.type === GuessEvent.type) {
-            const messageUser = <GuessEvent>cause
-            const { guess } = messageUser.details
+const server = new Edc.Server(port)
 
-            let event: Event<void, Guesses>
-            if (guess === correct) {
-                event = new RightGuessEvent().inherit(cause) // inherit() is key! it builds the "chain"
-            } else {
-                const wrong = new WrongGuessEvent().inherit(cause) // inherit() is key! it builds the "chain"
-                wrong.removeGuess(guess)
-                wrong.addAttempt()
-                event = wrong
-            }
+server.onError = async (cause) => {
+    console.log(cause.details.message)
+}
+server.onConnect = async (connection, auth: BasicAuth, arg3, that) => {
+    const instructions = `The goal of the game is to guess what word I am thinking of from the word bank.  Good luck!`
 
-            reply(event)
-        } else {
-            console.log(`Unknown event recieved: ${cause.type}`)
-            reply(new UnknownEventErrorEvent(cause))
-        }
-    },
-    onAck: async () => {},
-    onError: async (cause, info, reply, send) => {
-        console.log(cause.details.message)
-    },
-    onConnect: async (connection, auth: BasicAuth, arg3, that) => {
-        const instructions = `The goal of the game is to guess what word I am thinking of from the word bank.  Good luck!`
+    const start = new StartGameEvent('The Guessing Game', instructions, wordBank)
 
-        const start = new StartGameEvent('The Guessing Game', instructions, wordBank)
-
-        that.sendEvent(connection, start)
-    },
-    onClose: async () => {}
+    that.sendEvent(connection, start)
 }
 
-const server = new Edc.Server(port, serverHandlers)
+server.onEvent(GuessEvent.type, async (cause, ws, reply) => {
+    const messageUser = <GuessEvent>cause
+    const { guess } = messageUser.details
+
+    let event: Event<void, Guesses>
+    if (guess === correct) {
+        event = new RightGuessEvent().inherit(cause) // inherit() is key! it builds the "chain"
+    } else {
+        const wrong = new WrongGuessEvent().inherit(cause) // inherit() is key! it builds the "chain"
+        wrong.removeGuess(guess)
+        wrong.addAttempt()
+        event = wrong
+    }
+
+    reply(event)
+})
